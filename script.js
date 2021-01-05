d3c0c8623e5f414522380b4beb9d721bc0abd87f
rahulshahaha <getrshah@gmail.com>
2021-01-04 20:35:26 -0500

    j

--- script.js
+++ script.js
@@ -29,24 +29,16 @@ for(i = 0; i < 100; i++)
 
 }
 
-//document.body.appendChild(newSelect);
     
   optionChange();
-  
-
-// var e = document.getElementById("mySelect");
-// var strUser = e.options[e.selectedIndex].value;
-
-// console.log(strUser);
-
-// document.getElementById("preview").src = data.data.memes[parseInt(strUser)].url;
-// changeFavicon(data.data.memes[parseInt(strUser)].url);
 
     })
 
 function createBoxes(number){
   var container = document.getElementById("textholder");
 
+
+//Textboxes
 var child = container.lastElementChild;  
         while (child) { 
             container.removeChild(child); 
@@ -54,21 +46,102 @@ var child = container.lastElementChild;
         } 
 
 
-
-
   for (i=0; i<number;i++){
-    // Append a node with a random text
-    // Create an <input> element, set its type and name attributes
+
     var input = document.createElement("input");
     input.type = "text";
     input.id = i;
     container.appendChild(input);
     input.placeholder = "Text " + (i + 1);
-    // Append a line break 
-    //container.appendChild(document.createElement("br"));
-
 
   }
+
+
+//x positions
+
+container = document.getElementById("xPosition");
+
+child = container.lastElementChild;  
+        while (child) { 
+            container.removeChild(child); 
+            child = container.lastElementChild; 
+        } 
+
+  for (i=0; i<number;i++){
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.id = "x" + i;
+    container.appendChild(input);
+    input.placeholder = "(optional) X-Pos";
+
+  }
+
+
+
+  //y positions
+
+container = document.getElementById("yPosition");
+
+child = container.lastElementChild;  
+        while (child) { 
+            container.removeChild(child); 
+            child = container.lastElementChild; 
+        } 
+
+  for (i=0; i<number;i++){
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.id = "y" + i;
+    container.appendChild(input);
+    input.placeholder = "(optional) Y-Pos";
+
+  }
+
+
+//width
+
+container = document.getElementById("width");
+
+child = container.lastElementChild;  
+        while (child) { 
+            container.removeChild(child); 
+            child = container.lastElementChild; 
+        } 
+
+  for (i=0; i<number;i++){
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.id = "w" + i;
+    container.appendChild(input);
+    input.placeholder = "(optional) Width";
+
+  }
+
+
+  //height
+
+container = document.getElementById("height");
+
+child = container.lastElementChild;  
+        while (child) { 
+            container.removeChild(child); 
+            child = container.lastElementChild; 
+        } 
+
+  for (i=0; i<number;i++){
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.id = "h" + i;
+    container.appendChild(input);
+    input.placeholder = "(optional) Height";
+
+  }
+
+
 }
 
 function optionChange(){
@@ -86,6 +159,10 @@ numberOfBoxes = globalData.data.memes[parseInt(strUser)].box_count;
 createBoxes(globalData.data.memes[parseInt(strUser)].box_count);
 
 
+}
+
+function copyURL(){
+  Clipboard.write("texst");
 }
 
 function callAPI(){
@@ -113,8 +190,21 @@ params.append('text1',"g");
 //params.append('boxes[1][text]',"test2");
 
 for(i = 0; i < numberOfBoxes; i++){
+  //text boxes
   params.append('boxes['+i+'][text]',document.getElementById(i.toString()).value);
+
+
+  //x positions
+  params.append('boxes['+i+'][x]',parseInt(document.getElementById("x" + i).value));
+  //y positions
+  params.append('boxes['+i+'][y]',parseInt(document.getElementById("y" + i).value));
+  //width
+  params.append('boxes['+i+'][width]',parseInt(document.getElementById("w" + i).value));
+  //height
+  params.append('boxes['+i+'][height]',parseInt(document.getElementById("h" + i).value));
+
 }
+
 
 
 method = {method:'POST',body:params};
@@ -133,6 +223,62 @@ document.getElementById("preview").src = data.data.url;
     })
 }
 
+function addTextbox(){
+
+  if(numberOfBoxes == 5){
+    return;
+  }
+
+//Text
+var container = document.getElementById("textholder");
+var input = document.createElement("input");
+input.type = "text";
+input.id = numberOfBoxes;
+container.appendChild(input);
+input.placeholder = "Text " + (numberOfBoxes + 1);
+
+
+//X position
+container = document.getElementById("xPosition");
+input = document.createElement("input");
+input.type = "text";
+input.id = "x" + numberOfBoxes;
+container.appendChild(input);
+input.placeholder = "(optional) X-Pos"
+
+
+//Y position
+container = document.getElementById("yPosition");
+input = document.createElement("input");
+input.type = "text";
+input.id = "y" + numberOfBoxes;
+container.appendChild(input);
+input.placeholder = "(optional) y-Pos"
+
+
+//Width
+container = document.getElementById("width");
+input = document.createElement("input");
+input.type = "text";
+input.id = "w" + numberOfBoxes;
+container.appendChild(input);
+input.placeholder = "(optional) Width"
+
+
+//Hieght
+container = document.getElementById("height");
+input = document.createElement("input");
+input.type = "text";
+input.id = "h" + numberOfBoxes;
+container.appendChild(input);
+input.placeholder = "(optional) Height"
+
+
+
+numberOfBoxes += 1;
+
+
+}
 
 function changeFavicon(src) {
  var link = document.createElement('link'),
